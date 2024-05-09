c8ffde6fb00ce426613a6fa9c75b53c0635c67a0
Matej <matej.todorovsi1@gmail.com>
2024-05-09 13:27:11 +0200

    adding 4th labs

---
+++ integrated_systems/EShopAdminApplication/EShopAdminApplication/Models/EShopApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace EShopAdminApplication.Models
+{
+    public class EShopApplicationUser
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ integrated_systems/EShopAdminApplication/EShopAdminApplication/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShopAdminApplication.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string MovieName { get; set; }
+
+        public string MovieDescription { get; set; }
+
+        public string MovieImage { get; set; }
+        public double Rating { get; set; }
+    }
+}
